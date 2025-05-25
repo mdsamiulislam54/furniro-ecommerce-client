4d912579ca019272a2694a60c0ba8ebaac2dabb8
mdsamiulislam54 <samiulm5332@gmail.com>
2025-05-25 19:36:00 +0600

    add header section with responsive desgin add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useState } from "react";
+import logo from "../../assets/logo/logo.png";
+import manIcons from "../../assets/icons/man.png";
+import searchIcons from "../../assets/icons/serach.png";
+import heartIcons from "../../assets/icons/heart.png";
+import cartIcons from "../../assets/icons/cart.png";
+import { NavLink } from "react-router";
+import { HiMenuAlt2 } from "react-icons/hi";
+import Mobile from "./Mobile";
 
 const Header = () => {
-  return (
-    <nav>
-        
-    </nav>
-  )
-}
+  const [isShowMobileMenu, setShowMobileMenu] = useState(false);
 
-export default Header
+  return (
+    <nav className="py-6 bg-white shadow-md relative">
+      <div className="flex justify-between items-center  w-11/12 mx-auto ">
+        <div>
+          <img src={logo} alt="logo" className="w-[185px] h-[35px]" />
+        </div>
+        <div className="lg:block hidden">
+          <ul className=" flex items-center text-gray-700 gap-6">
+            <NavLink className="text-lg font-medium tracking-wide ">
+              Home
+            </NavLink>
+            <NavLink className="text-lg font-medium tracking-wide ">
+              Shop
+            </NavLink>
+            <NavLink className="text-lg font-medium tracking-wide ">
+              About
+            </NavLink>
+            <NavLink className="text-lg font-medium tracking-wide ">
+              Contact
+            </NavLink>
+            <NavLink className="text-lg font-medium tracking-wide ">
+              Blogs
+            </NavLink>
+          </ul>
+        </div>
+        <div>
+          <div className="flex gap-4 items-center max-lg:hidden">
+            <img src={searchIcons} alt="Search" className="w-7 h-7" />
+            <img src={heartIcons} alt="Wishlist" className="w-7 h-7" />
+            <img src={cartIcons} alt="Cart" className="w-7 h-7" />
+            <img src={manIcons} alt="User" className="w-7 h-7" />
+          </div>
+          <div>
+            <button
+              onClick={() => setShowMobileMenu(!isShowMobileMenu)}
+              className="lg:hidden block text-2xl text-gray-700 cursor-pointer"
+            >
+              <HiMenuAlt2 size={30}/>
+            </button>
+          </div>
+        </div>
+
+        
+          <div className={`absolute top-[0px] right-[-999px]    shadow-md w-8/12  bg-secondary md:hidden min-h-screen ${isShowMobileMenu ? "right-[0px]" : ""} transform transition-all duration-500 ease-in-out`}>
+            <Mobile  setShowMobileMenu={setShowMobileMenu} />
+          </div>
+       
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/Mobile.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import manIcons from "../../assets/icons/man.png";
+import searchIcons from "../../assets/icons/serach.png";
+import heartIcons from "../../assets/icons/heart.png";
+import cartIcons from "../../assets/icons/cart.png";
+import { NavLink } from "react-router";
+import { IoClose } from "react-icons/io5";
+const Mobile = ({setShowMobileMenu}) => {
+  return (
+    <div>
+      <div className="p-10  relative">
+        <di className="absolute top-5 right-10 flex justify-end">
+          <button
+            onClick={() => setShowMobileMenu(false)}
+            className="text-2xl  bg-primary rounded-full p-1 font-bold cursor-pointer text-white "
+          >
+         <IoClose size={30 } className=""/>
+          </button>
+        </di>
+        <ul className=" flex flex-col text-gray-700 gap-6">
+          <NavLink className="text-lg font-medium tracking-wide ">Home</NavLink>
+          <NavLink className="text-lg font-medium tracking-wide ">Shop</NavLink>
+          <NavLink className="text-lg font-medium tracking-wide ">
+            About
+          </NavLink>
+          <NavLink className="text-lg font-medium tracking-wide ">
+            Contact
+          </NavLink>
+          <NavLink className="text-lg font-medium tracking-wide ">
+            Blogs
+          </NavLink>
+        </ul>
+         <div className="flex flex-wrap gap-8 items-center mt-5 ">
+        <img src={searchIcons} alt="Search" className="w-7 h-7" />
+        <img src={heartIcons} alt="Wishlist" className="w-7 h-7" />
+        <img src={cartIcons} alt="Cart" className="w-7 h-7" />
+        <img src={manIcons} alt="User" className="w-7 h-7" />
+      </div>
+      </div>
+     
+    </div>
+  );
+};
+
+export default Mobile;
